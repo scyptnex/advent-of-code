2cbe787c93aff18864d96cac748ec8e09211fa9f
Scyptnex <nic.h.misc@gmail.com>
2022-12-01 20:50:07 +1100

    day 1

---
+++ aoc/elf_calories.py
@@ -0,0 +1,22 @@
+import sys
+
+def main():
+    cur_elf = 0
+    elves = []
+    for l in sys.stdin:
+        line = l[:-1]
+        if not line:
+            elves.append(cur_elf)
+            cur_elf = 0
+        else:
+            cur_elf += int(line)
+    elves.append(cur_elf)
+
+    elves.sort()
+    print(elves)
+    print(elves[-1])
+    print(elves[-3] + elves[-2] +elves[-1])
+
+
+if __name__ == "__main__":
+    main()
