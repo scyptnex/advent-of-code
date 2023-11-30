116b6bf622b84f396a4d25de4da36b0aa6535df8
Scyptnex <nic.h.misc@gmail.com>
2023-11-30 20:55:55 +1100

    fix begin

--- begin.sh
+++ begin.sh
@@ -6,8 +6,8 @@ DAY=`date +%d`
 YEAR=2023
 PROJECT_DIR=$(dirname `readlink -f $0`)
 
-xdg-open https://adventofcode.com/$YEAR/day/$DAY
-xdg-open https://adventofcode.com/$YEAR/day/$DAY/input 
+xdg-open https://adventofcode.com/$YEAR/day/${DAY#0}
+xdg-open https://adventofcode.com/$YEAR/day/${DAY#0}/input 
 
 TODAY=$PROJECT_DIR/src/bin/p${DAY}.rs
 [ -f $TODAY ] || cp $PROJECT_DIR/src/bin/p00.rs $TODAY
