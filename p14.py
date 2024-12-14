de16c3e0301ec12fb0df485a6a44f06a1c7fab3d
Scyptnex <nic.h.misc@gmail.com>
2024-12-14 18:07:59 +1100

    yay information theory to the rescue

--- p14.py
+++ p14.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 class rbt:
 
@@ -28,12 +29,52 @@ class rbt:
             ret += 2
         return ret
 
+    def move(self, w, h):
+        self.x = (self.x + w + self.vx)%w
+        self.y = (self.y + h + self.vy)%h
+
 
 rbts = [rbt(l.strip()) for l in sys.stdin.readlines()]
+wi = 101
+he = 103
 
 quads = [0]*5
 for r in rbts:
-    quads[r.adv(100, 101, 103)] += 1
+    quads[r.adv(100, wi, he)] += 1
 
-print(quads)
 print(quads[0]*quads[1]*quads[2]*quads[3])
+
+def show(i, m, wi, he):
+    for y in range(he):
+        for x in range(wi):
+            print('X ' if m[x][y] else '  ', end='')
+        print()
+    print(i, '----------------------')
+
+def oddsof(rbts, wi, he):
+    sw = wi//5
+    sh = he//5
+    counts = [[0 for _ in range(6)]for _ in range(6)]
+    for r in rbts:
+        counts[r.x//sw][r.y//sh] += 1
+    expected = len(rbts)/25
+    sm = 0
+    for a in range(5):
+        for b in range(5):
+            nom = counts[a][b] - expected
+            nom = nom*nom
+            sm += nom / expected
+    return sm
+
+
+biggest = 0
+for i in range(0, 99999999):
+    e = oddsof(rbts, wi, he)
+    if e > biggest:
+        biggest = e
+        m = [[0 for _ in range(he)] for _ in range(wi)]
+        for r in rbts:
+            m[r.x][r.y] += 1
+        show(i, m, wi, he)
+    for r in rbts:
+        r.move(wi, he)
