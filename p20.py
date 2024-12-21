793c5ecaf6399513cab8183c8a7346f6edf0bbe1
Scyptnex <nic.h.misc@gmail.com>
2024-12-21 16:49:31 +1100

    no solution for p20 yet...

--- p20.py
+++ p20.py
@@ -12,59 +12,107 @@ def adj(track, cur):
     if c < len(track[r])-1:
         yield (r, c+1)
 
-def dijk(track, start, end, lim):
+def cheatable_from(track, dist, cstart):
     q = PriorityQueue()
-    v = set()
+    q.put((0, cstart, True))
+    v = {}
+    while not q.empty():
+        cost, cur, last_on_open = q.get()
+        if cur in v:
+            oldc, oldl = v[cur]
+            if not oldl or last_on_open:
+                continue
+        v[cur] = (cost, last_on_open)
+        on_open = track[cur[0]][cur[1]] != '#'
+        if on_open and not last_on_open:
+            yield (cur, cost)
+        if cost == dist:
+            continue
+        for a in adj(track, cur):
+            if cost > 0 or track[a[0]][a[1]] == '#':
+                #if cstart == (1, 2) and a == (9, 3):
+                #    print("__", cost, cur, last_on_open, on_open, a)
+                q.put((cost+1, a, on_open))
+
+def cheatable_front(track, dist, cstart):
+    frontier = [cstart]
+    v = set(frontier)
+    for cost in range(1, dist+1):
+        newf = []
+        for f in frontier:
+            for a in adj(track, f):
+                if a in v:
+                    continue
+                v.add(a)
+                tp = track[a[0]][a[1]]
+                if cost == 1 and tp != '#':
+                    continue
+                if tp != '#':
+                    yield (a, cost)
+                newf.append(a)
+        frontier = newf
+
+
+def cheats(track, dist, savings, cstart, s2e, e2s, limit):
+    for cend, ctime in cheatable_from(track, dist, cstart):
+        if cend not in e2s:
+            continue
+        total = s2e[cstart] + ctime + e2s[cend]
+        if total >= limit:
+            continue
+        if (limit - total) == 66:
+            print(limit, total, cstart, cend, s2e[cstart] , ctime , e2s[cend])
+        savings[(cstart, cend)] = limit - total
+
+
+
+def dijk(track, start):
+    q = PriorityQueue()
+    dists = {}
     q.put((0, start))
     while not q.empty():
         cost, cur = q.get()
-        if cur in v or cost > lim:
+        if cur in dists:
             continue
-        v.add(cur)
-        if cur == end:
-            return cost
+        dists[cur] = cost
         for a in adj(track, cur):
             if track[a[0]][a[1]] == '#':
                 continue
-            if track[cur[0]][cur[1]] == '1' and track[a[0]][a[1]] != '2':
-                continue
             q.put((cost+1, a))
-    return -1
+    return dists
 
 def go():
     track=[[c for c in l.strip()] for l in sys.stdin.readlines()]
-    
     for ri, row in enumerate(track):
         for ci, c in enumerate(row):
             if c == 'S':
                 start=(ri, ci)
             if c == 'E':
                 end=(ri, ci) 
-    normal = dijk(track, start, end, len(track)*len(track[0]))
-    savings = {}
-    for ri, row in enumerate(track):
-        print('.')
-        for ci, c in enumerate(row):
-            if track[ri][ci] != '#':
-                continue
-            for pa in adj(track, (ri, ci)):
-                if track[pa[0]][pa[1]] == '#':
-                    continue
-                track[ri][ci] = '1'
-                track[pa[0]][pa[1]] = '2'
-                new = dijk(track, start, end, normal)
-                track[ri][ci] = '#'
-                track[pa[0]][pa[1]] = '.'
-                if new != -1 and new < normal:
-                    sav = normal - new
-                    if sav not in savings:
-                        savings[sav] = 0
-                    savings[sav] += 1
-    totsav = 0
-    for s in savings:
-        if s < 100:
+    s2e = dijk(track, start)
+    e2s = dijk(track, end)
+    limit = s2e[end]
+    savings={}
+    savingss={}
+    for cstart in s2e:
+        if s2e[cstart] >= limit:
             continue
-        totsav += savings[s]
-    print(totsav)
+        cheats(track, 2, savings, cstart, s2e, e2s, limit) 
+        cheats(track, 20, savingss, cstart, s2e, e2s, limit) 
+    def desc(sv):
+        gs = {}
+        gt100 = 0
+        for s in sv:
+            s = sv[s]
+            if s not in gs:
+                gs[s] = 0
+            gs[s] += 1
+            if s >= 100:
+                gt100 += 1
+        #for s in sorted(gs.keys()):
+        #    print(s, gs[s])
+        print(gt100)
+    desc(savings)
+    desc(savingss)
 
 go()
