9fd4e868e12898ef3e27e5a52c17bed5470a1e6a
Scyptnex <nic.h.misc@gmail.com>
2025-12-14 19:57:36 +1100

    can't believe that worked

--- 25/src/bin/p09.rs
+++ 25/src/bin/p09.rs
@@ -37,18 +37,6 @@ fn solve_2(input: &str) -> u64 {
         .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap()))
         .collect::<Vec<Til>>();
 
-    let mut minx: i64 = coords[0].0;
-    let mut miny: i64 = coords[0].1;
-    let mut maxx: i64 = minx;
-    let mut maxy: i64 = miny;
-    for c in &coords {
-        minx = std::cmp::min(minx, c.0 - 1);
-        miny = std::cmp::min(miny, c.1 - 1);
-        maxx = std::cmp::max(maxx, c.0 + 1);
-        maxy = std::cmp::max(maxy, c.1 + 1);
-    }
-    dbg!(minx, miny, maxx, maxy);
-
     let mut gs = Tilez::new();
     for i in 0..coords.len() {
         let j = (i + 1) % coords.len();
@@ -60,53 +48,94 @@ fn solve_2(input: &str) -> u64 {
             }
         }
     }
+    let gs = gs;
     dbg!(gs.len());
 
-    let mut outside = Tilez::new();
+    let adjs = gs
+        .iter()
+        .flat_map(|t| adj().map(|a| adt(&a, t)))
+        .collect::<Tilez>();
+
+    let perims = adjs.difference(&gs).copied().collect::<Tilez>();
+
+    let arbitrary_p = *perims.iter().next().unwrap();
+    let mut side_a = Tilez::new();
     let mut front = Vec::<Til>::new();
-    front.push((minx, miny));
-    outside.insert((minx, miny));
+    front.push(arbitrary_p);
+    side_a.insert(arbitrary_p);
     while let Some(f) = front.pop() {
         for a in adj().map(|a| adt(&a, &f)) {
-            if outside.contains(&a) || gs.contains(&a) {
-                continue;
-            }
-            if a.0 < minx || a.0 > maxx || a.1 < miny || a.1 > maxy {
+            if side_a.contains(&a) || gs.contains(&a) || !perims.contains(&a) {
                 continue;
             }
             front.push(a);
-            outside.insert(a);
+            side_a.insert(a);
         }
     }
-    dbg!(outside.len());
+    let side_b = perims.difference(&side_a).copied().collect::<Tilez>();
+    let min_a = side_a.iter().map(|t| t.0).min();
+    let min_b = side_b.iter().map(|t| t.0).min();
+    let outside = match min_a < min_b {
+        true => side_a,
+        false => side_b,
+    };
+    dbg!(min_a, min_b);
 
-    let mut inside = Tilez::new();
-    for x in minx..maxx {
-        for y in miny..maxy {
-            let c = (x, y);
-            if !outside.contains(&c) {
-                inside.insert(c);
-            }
-        }
-    }
-    dbg!(inside.len());
-
+    let area = |(i, j)| -> i64 {
+        let (xi, yi): (i64, i64) = coords[i];
+        let (xj, yj): (i64, i64) = coords[j];
+        ((xi - xj).abs() + 1) * ((yi - yj).abs() + 1)
+    };
     (0..coords.len() - 1)
         .flat_map(|i| (i + 1..coords.len()).map(move |j| (i, j)))
-        .flat_map(|(i, j)| {
-            let (xi, yi): (i64, i64) = coords[i];
-            let (xj, yj): (i64, i64) = coords[j];
-            for x in std::cmp::min(xi, xj)..std::cmp::max(xi, xj) + 1 {
-                for y in std::cmp::min(yi, yj)..std::cmp::max(yi, yj) + 1 {
-                    if outside.contains(&(x, y)) {
-                        return None;
-                    }
-                }
-            }
-            Some(((xi - xj).abs() + 1) * ((yi - yj).abs() + 1))
+        .map(|(i, j)| (i, j, area((i, j))))
+        .filter(|(i, j, _)| {
+            let (xi, yi): (i64, i64) = coords[*i];
+            let (xj, yj): (i64, i64) = coords[*j];
+            (std::cmp::min(xi, xj)..=std::cmp::max(xi, xj))
+                .map(|x| (x, yi))
+                .all(|t| !outside.contains(&t))
+                && (std::cmp::min(xi, xj)..=std::cmp::max(xi, xj))
+                    .map(|x| (x, yj))
+                    .all(|t| !outside.contains(&t))
+                && (std::cmp::min(yi, yj)..=std::cmp::max(yi, yj))
+                    .map(|y| (xi, y))
+                    .all(|t| !outside.contains(&t))
+                && (std::cmp::min(yi, yj)..=std::cmp::max(yi, yj))
+                    .map(|y| (xj, y))
+                    .all(|t| !outside.contains(&t))
         })
+        .map(|g| g.2)
         .max()
         .unwrap() as u64
+
+    // let mut inside = Tilez::new();
+    // for x in minx..maxx {
+    //     for y in miny..maxy {
+    //         let c = (x, y);
+    //         if !outside.contains(&c) {
+    //             inside.insert(c);
+    //         }
+    //     }
+    // }
+    // dbg!(inside.len());
+
+    // (0..coords.len() - 1)
+    //     .flat_map(|i| (i + 1..coords.len()).map(move |j| (i, j)))
+    //     .flat_map(|(i, j)| {
+    //         let (xi, yi): (i64, i64) = coords[i];
+    //         let (xj, yj): (i64, i64) = coords[j];
+    //         for x in std::cmp::min(xi, xj)..std::cmp::max(xi, xj) + 1 {
+    //             for y in std::cmp::min(yi, yj)..std::cmp::max(yi, yj) + 1 {
+    //                 if outside.contains(&(x, y)) {
+    //                     return None;
+    //                 }
+    //             }
+    //         }
+    //         Some(((xi - xj).abs() + 1) * ((yi - yj).abs() + 1))
+    //     })
+    //     .max()
+    //     .unwrap() as u64
 }
 
 fn main() {
